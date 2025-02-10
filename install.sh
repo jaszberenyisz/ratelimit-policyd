fbac182c66b1b77e728a3ef7db1f151d64f9c91f
Szabolcs Jaszberenyi <12271239+jaszberenyisz@users.noreply.github.com>
2025-02-10 09:23:42 +0100

    Update install.sh
    Added systemd file copying for debian support

--- install.sh
+++ install.sh
@@ -13,5 +13,11 @@ chmod 755 daemon.pl init.d/ratelimit-policyd
 ln -sf "$DIR/init.d/ratelimit-policyd" /etc/init.d/
 insserv ratelimit-policyd
 
+# systemd files
+rm -f /lib/systemd/system/ratelimit.service
+cp "$DIR/lib/systemd/system/ratelimit.service" "/lib/systemd/system/"
+rm -f /etc/systemd/system/ratelimit.service
+ln -s "/lib/systemd/system/ratelimit.service" "/etc/systemd/system/"
+
 # install logrotation configuration
 ln -sf "$DIR/logrotate.d/ratelimit-policyd" /etc/logrotate.d/
